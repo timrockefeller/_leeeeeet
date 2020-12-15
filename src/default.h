ade6486881c4a295e64d0b5758a1ebf366c12b9a
kitekii <tim720719@163.com>
2020-12-15 16:00:20 +0800

    add default include file

---
+++ src/default.h
@@ -0,0 +1,27 @@
+////////////////////////////////
+// default include env
+
+/// IO
+#include <iostream>
+#include <fstream>
+
+
+/// STL
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <type_traits>
+
+/// MEMORY
+#include <memory>
+#include <mutex>
+
+
+
+
+
+// 
+////////////////////////////////
